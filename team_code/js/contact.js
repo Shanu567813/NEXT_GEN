7dbe19838dd7391ad05c69f0815021317d0c16eb
Deepak <dk01453081435@gmail.com>
2026-08-17 16:28:12 +0530

    commit 1

--- team_code/js/contact.js
+++ team_code/js/contact.js
@@ -1,5 +1,9 @@
 //COMMIT 1 START: 
-
+// Contact page initialization & logger
+// Contact page initialization & logger
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("📧 Contact Page JS successfully loaded!");
+});
 //COMMIT 1 END: 
 
 
